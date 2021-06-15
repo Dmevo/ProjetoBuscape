9497321aa6ece88ca2e06e18db5ac47fdff0f355
Danilo Vasconcellos <danilodemv@hotmail.com>
2021-06-14 22:49:20 -0300

    Finalizando estilização e toques finais

--- application/controller/home_controller.py
+++ application/controller/home_controller.py
@@ -3,18 +3,16 @@ from application import app
 from application.model.entity.produto import Produto
 from application.model.dao.produtoDAO import ProdutoDAO
 
+produto_dao = ProdutoDAO()
+produto_list = produto_dao.listar_todos()
+carrinho_list = produto_dao.mostrar_carrinho()
+
 @app.route("/")
 def home():
-    produto_dao = ProdutoDAO()
-    produto_list = produto_dao.listar_todos()
-    carrinho_list = produto_dao.mostrar_carrinho()
     return render_template("home.html", carrinho_list=carrinho_list, produto_list=produto_list)
 
 @app.route("/add/<id>")
 def add(id):
-    produto_dao = ProdutoDAO()
-    produto_list = produto_dao.listar_todos()
-    carrinho_list = produto_dao.mostrar_carrinho()
     for produto in produto_list:
         if  produto.get_id() == int(id):
             carrinho_list.append(produto) 
@@ -22,9 +20,6 @@ def add(id):
 
 @app.route("/delete/<id>")
 def delete(id):
-    produto_dao = ProdutoDAO()
-    produto_list = produto_dao.listar_todos()
-    carrinho_list = produto_dao.mostrar_carrinho()
     for produto in carrinho_list:
         if produto.get_id() == int(id):
             carrinho_list.remove(produto)
